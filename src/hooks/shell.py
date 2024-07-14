342671f4bb1bedb9278c9d20211dae79581fd897
Jackie Bhadange <jackieb108@gmail.com>
2024-07-14 12:12:44 -0400

    Update shell.py

--- src/hooks/shell.py
+++ src/hooks/shell.py
@@ -1,1 +1,8 @@
-print('printed in python')
+#print('printed in python')
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the overwriting:
+#f = open("demofile3.txt", "r")
+#print(f.read())
